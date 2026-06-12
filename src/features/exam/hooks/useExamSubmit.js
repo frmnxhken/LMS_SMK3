c99efc2884ce7476bfac82b22c6a6491b5dfc7cf
frmnxhken <frmnxhken@gmail.com>
2026-06-12 09:50:21 +0700

    Add invalidate query exam

--- src/features/exam/hooks/useExamSubmit.js
+++ src/features/exam/hooks/useExamSubmit.js
@@ -1,14 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import { submitExam } from "../api/examApi";
 import { useNavigate } from "react-router";
 
 const useExamSubmit = (id_class, id_exam) => {
   const [errors, setErrors] = useState(null);
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const mutation = useMutation({
     mutationFn: (payload) => submitExam(id_class, id_exam, payload),
     onSuccess: () => {
+      queryClient.invalidateQueries(["examDetail", id_class, id_exam]);
       navigate(`/course/${id_class}/exam`);
     },
   });
