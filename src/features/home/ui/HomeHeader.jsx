57c99272bb12806049662cc672c326340b8ee515
frmnxhken <frmnxhken@gmail.com>
2026-07-02 08:54:31 +0700

    Rev: Display academic year on homepage

--- src/features/home/ui/HomeHeader.jsx
+++ src/features/home/ui/HomeHeader.jsx
@@ -1,14 +1,22 @@
+import { useAcademicYear } from "@/app/contexts/AcademicYearContext";
 import React from "react";
 
 const HomeHeader = ({ role }) => {
+  const { academicYear } = useAcademicYear();
+
   return (
     <div className="mb-4">
-      <h1 className="text-heading font-bold text-xl">Selamat Datang</h1>
-      <p className="text-sm text-text-muted">
-        {role === "student"
-          ? "Siap belajar hal baru hari ini?"
-          : "Siap membagikan hal baru hari ini?"}
-      </p>
+      <div className="flex justify-between items-center">
+        <div>
+          <h1 className="text-heading font-bold text-xl">Selamat Datang</h1>
+          <p className="text-sm text-text-muted">
+            {role === "student"
+              ? "Siap belajar hal baru hari ini?"
+              : "Siap membagikan hal baru hari ini?"}
+          </p>
+        </div>
+        <p className="text-sm font-semibold">Tahun Ajaran: {academicYear}</p>
+      </div>
     </div>
   );
 };
